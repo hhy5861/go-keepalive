3f36a093fb7482ed376f97c58aa692e582293450
Mike <huanghaiying@bigbaser.com>
2017-05-26 19:06:10 +0800

    first commit

---
+++ keepalive.go
@@ -0,0 +1,53 @@
+package utility
+
+import (
+	"net/http"
+	"time"
+	"net"
+	"io/ioutil"
+)
+
+type NewClient struct {
+	Maxconn        int
+	MaxDialTimeOut int
+	RepTimeOut     int
+	client         *http.Client
+}
+
+func (cfg *NewClient) GetClient() *http.Client {
+	if cfg.client == nil {
+		cfg.client = &http.Client{
+			Transport: &http.Transport{
+				ResponseHeaderTimeout: time.Millisecond * time.Duration(cfg.RepTimeOut),
+				MaxIdleConnsPerHost:   cfg.Maxconn,
+				Dial: func(netw, addr string) (net.Conn, error) {
+					conn, err := net.DialTimeout(netw, addr, time.Millisecond*time.Duration(cfg.MaxDialTimeOut))
+					if err == nil {
+						if tcpConn, ok := conn.(*net.TCPConn); ok {
+							tcpConn.SetLinger(0)
+						}
+					}
+
+					return conn, err
+				},
+			},
+		}
+	}
+
+	return cfg.client
+}
+
+func (cfg *NewClient) Request(req *http.Request) ([]byte, error) {
+	resp, err := cfg.GetClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
